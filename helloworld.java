efde34343af7c2060447509de98c219294e4d11a
KnightJR8413 <KnightJR845@proton.me>
2025-03-14 11:07:14 +1100

    helloworld file deleted

--- helloworld.java
+++
@@ -1,9 +0,0 @@
-//test
-//first change
-//second change
-public class helloworld {
-    public static void main(String[] args){
-        System.out.println("Hello Software Development");
-    }
-}
-//1034
