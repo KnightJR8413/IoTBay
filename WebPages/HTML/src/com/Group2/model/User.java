5956c44a7182096f3aa80221540249b6a835eea3
JHTech <160552777+JacksonHtech@users.noreply.github.com>
2025-03-28 12:12:25 +1100

    Create User.java

---
+++ WebPages/HTML/src/com/Group2/model/User.java
@@ -0,0 +1,32 @@
+package com.Group2.model;
+
+public class User {
+    private String username;
+    private String email;
+
+    // Constructor
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getter for username
+    public String getUsername() {
+        return username;
+    }
+
+    // Setter for username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter for email
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter for email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
